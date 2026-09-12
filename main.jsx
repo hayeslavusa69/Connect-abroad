f0d035fcd8aa6be4910090abde7ffe84463c5fed
hayeslavusa69 <hayeslavusa69@gmail.com>
2026-09-12 21:06:55 +0300

    Delete main.jsx

--- main.jsx
+++
@@ -1,3 +0,0 @@
-import React from 'react'; import {createRoot} from 'react-dom/client'; import {createClient} from '@supabase/supabase-js'; import App from './App'; import './index.css';
-export const supabase=createClient('https://jkbsyccqufxkqogedsbx.supabase.co','sb_publishable_oI13K7ppEYzknUhDCO1wUA_Y-TDeOPn');
-createRoot(document.getElementById('root')).render(<React.StrictMode><App/></React.StrictMode>);
